65866a517df8e6d4712a5acdf8d8d02aaddf6698
lal129 <113038061+lal129@users.noreply.github.com>
2022-10-31 00:45:10 -0400

    Challenge A

--- app/unemployment.py
+++ app/unemployment.py
@@ -15,3 +15,10 @@ response = requests.get(request_url)
 parsed_response = json.loads(response.text)
 print(type(parsed_response))
 pprint(parsed_response)
+
+# Challenge A
+# What is the most recent unemployment rate? And the corresponding date? Display the unemployment rate using a percent sign.
+
+# used a breakpoint() to find info
+latest = parsed_response["data"][0]
+print(latest)
